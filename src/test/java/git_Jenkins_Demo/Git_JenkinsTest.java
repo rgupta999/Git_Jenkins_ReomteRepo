14e31372e9a0e0ebbdb4c27c371f7026d10c3770
Ranjan <granjan1443@gmail.com>
2020-08-17 19:57:55 +0530

    my nth commit

--- src/test/java/git_Jenkins_Demo/Git_JenkinsTest.java
+++ src/test/java/git_Jenkins_Demo/Git_JenkinsTest.java
@@ -6,13 +6,15 @@ import org.testng.annotations.Test;
 public class Git_JenkinsTest extends Git_Jenkins_TestBase{
 	
 	@Test
-	public void signUp()
+	public void signUp() throws InterruptedException
 	{
 		driver.findElement(By.name("email")).sendKeys("granjan");
 		driver.findElement(By.name("pass")).sendKeys("gupta");
-		driver.findElement(By.name("firstname")).sendKeys("ranjan");
-		driver.findElement(By.name("lastname")).sendKeys("gupta");
-		driver.findElement(By.name("websubmit")).click();
+		driver.findElement(By.name("login")).click();
+		//driver.findElement(By.name("firstname")).sendKeys("ranjan");
+		//driver.findElement(By.name("lastname")).sendKeys("gupta");
+		
+		
 	}
 	
 	
