3135a174fd51775dfcba05fa21d927124f785559
Ranjan <granjan1443@gmail.com>
2020-08-17 19:01:00 +0530

    my uadated commit

--- src/test/java/git_Jenkins_Demo/Git_JenkinsTest.java
+++ src/test/java/git_Jenkins_Demo/Git_JenkinsTest.java
@@ -8,7 +8,7 @@ public class Git_JenkinsTest extends Git_Jenkins_TestBase{
 	@Test
 	public void signUp()
 	{
-		driver.findElement(By.name("email")).sendKeys("ranjan");
+		driver.findElement(By.name("email")).sendKeys("granjan");
 		driver.findElement(By.name("pass")).sendKeys("gupta");
 		driver.findElement(By.name("firstname")).sendKeys("ranjan");
 		driver.findElement(By.name("lastname")).sendKeys("gupta");
